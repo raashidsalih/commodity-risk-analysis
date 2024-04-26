124ce3c0f13480480a0cdc6b4441b72bb93bfb30
Raashid Salih <61780547+raashidsalih@users.noreply.github.com>
2024-04-26 17:56:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,8 +45,6 @@ def plot_sentiment_over_time(df):
 keywords_df = pd.read_csv('keywords.csv')
 commodities = keywords_df.columns.tolist()
 
-st.write(pd.read_excel("./data/Steel_final.xlsx"))
-
 st.sidebar.markdown("<h1 style='text-align: center; color: grey;'>FreightFox</h1>", unsafe_allow_html=True)
 st.sidebar.markdown("<h1 style='text-align: center; color: white;'>Commodity Risk Analysis</h1>", unsafe_allow_html=True)
 # Dropdown to choose a commodity
